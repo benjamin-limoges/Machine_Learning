dfc2bdcca2df124d086e8e1b94a05d86687bf308
Real Limoges <benjamin.limoges@outlook.com>
2015-02-08 18:46:57 -0500

    Write to file rather than terminal

--- Kaggle/Forest_Cover/foo.py
+++ Kaggle/Forest_Cover/foo.py
@@ -7,6 +7,7 @@ from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 
 data = pd.read_csv( 'train.csv', header = 0 )
+outfile = open("output.txt", 'w+')
 features = data.columns.values.tolist()
 label = features.pop()
 
@@ -15,21 +16,27 @@ X_train, X_test, y_train, y_test = train_test_split(
     data[features], data[label], test_size=0.33, random_state=0)
 
 # Set the parameters
+tuned_parameters = [{'n_estimators': [10, 100],
+					 'max_features': ['sqrt', 'log2', None],
+					 'max_depth': [None, 3, 5],
+					 'n_jobs': [4]}]
 
+objects = [RandomForestClassifier()]
 scores = ['precision', 'recall']
 
 for score in scores:
-    print("# Tuning hyper-parameters for %s" % score)
-    print()
+    outfile.write("# Tuning hyper-parameters for %s\n\n" % score)
 
-    clf = GridSearchCV(RandomForestClassifier(), tuned_parameters, 
+    clf = GridSearchCV(objects[0], tuned_parameters, 
     				   cv=5, scoring=score)
     clf.fit(X_train, y_train)
 
-    print("Best parameters set found on training set:\n")
-    print(clf.best_estimator_)
-    print("\n")
-    print("Grid scores on training set:\n")
+    outfile.write("Best parameters set found on training set:\n\n")
+    outfile.write(str(clf.best_estimator_))
+    outfile.write("\n")
+    outfile.write("Grid scores on training set:\n")
     for params, mean_score, scores in clf.grid_scores_:
-        print("%0.3f (+/-%0.03f) for %r"
-              % (mean_score, scores.std() / 2, params))
+        outfile.write("%0.3f (+/-%0.03f) for %r"
+                      % (mean_score, scores.std() / 2, params))
+        outfile.write("\n")
+    outfile.write("\n")
