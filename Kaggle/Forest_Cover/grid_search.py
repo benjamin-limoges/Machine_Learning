38046cb3edd0c63110062c90002bfe0e693c9dfe
Real Limoges <benjamin.limoges@outlook.com>
2015-02-09 08:37:52 -0500

    Refactor code, improve readability

--- Kaggle/Forest_Cover/grid_search.py
+++ Kaggle/Forest_Cover/grid_search.py
@@ -4,18 +4,26 @@ from sklearn.cross_validation import train_test_split
 from sklearn.grid_search import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 
+import global_vars
+import os
+
 import pandas as pd
+
+DATA_FILE = "train.csv"
 
 def read_data():
 
-    data = pd.read_csv( 'train.csv', header = 0 )
+
+    data = pd.read_csv( os.path.join( global_vars.data_dir, DATA_FILE),
+                        header = 0 )
     features = data.columns.values.tolist()
     label = features.pop()
 
     return data, features, label
 
-def search(model, parameters):
-    outfile = open("outfile.txt", 'w+')
+def search(model, parameters, filename):
+    filename = filename + "_output.txt"
+    outfile = open(os.path.join(global_vars.output_dir, filename), 'w+')
     data, features, label = read_data()
 
     # Split the dataset
@@ -27,12 +35,10 @@ def search(model, parameters):
     for score in scores:
         outfile.write("# Tuning hyper-parameters for %s\n\n" % score)
 
-        clf = GridSearchCV(model, tuned_parameters, 
-                       cv=5, scoring=score)
+        clf = GridSearchCV(model, parameters, cv=5, scoring=score)
         clf.fit(X_train, y_train)
 
         outfile.write("Best parameters set found on training set:\n\n")
-        print (type(clf.best_estimator_))
         outfile.write(str(clf.best_estimator_))
         outfile.write("\n")
         outfile.write("Grid scores on training set:\n")
@@ -42,18 +48,5 @@ def search(model, parameters):
             outfile.write("\n")
         outfile.write("\n")
 
+    outfile.close()
     return clf.best_estimator_
-
-
-tuned_parameters = [{'n_estimators': [10, 100],
-                     'max_features': ['sqrt', 'log2', None],
-                     'max_depth': [None, 3, 5],
-                     'n_jobs': [4]}]
-
-x = RUNME(RandomForestClassifier(), tuned_parameters)
-
-data, features, label = read_data()
-X_train, X_test, y_train, y_test = train_test_split(
-        data[features], data[label], test_size=0.33, random_state=0)
-clf = x.fit(X_train, y_train)
-print(clf.score(X_test, y_test))
